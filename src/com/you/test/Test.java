2bda0eecbb4b23e115dff0fd18db57bd58d02cec
Administrator <Administrator@YeQi>
2019-06-24 22:38:01 +0800

    0.1版本测试Demo

--- src/com/you/test/Test.java
+++ src/com/you/test/Test.java
@@ -2,6 +2,6 @@ package com.you.test;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("嘿嘿，这是我测试的Demo");
+		System.out.println("嘿嘿，这是我测试的Demo,,我正在修改自己的Demo哦");
 	}
 }	
